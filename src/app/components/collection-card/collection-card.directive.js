7cc10f96ade0c6737d14a32699e6e4f135343407
Lars <lbersch@gwdg.de>
2015-06-11 22:29:27 +0200

    Improved Collection Card on Abstract Page

--- src/app/components/collection-card/collection-card.directive.js
+++ src/app/components/collection-card/collection-card.directive.js
@@ -4,7 +4,7 @@ angular.module('crucio')
   .directive('crCollectionCard', function() {
     return {
       restrict: 'E',
-      scope: { collection: '=', collectionid: '=' },
+      scope: { collection: '=', collectionid: '=', exam: '=' },
       controller: function($scope, $location, $mdToast, Collection) {
         $scope.getUserDataLength = function(question_id_list, user_datas) {
           var result = 0;
@@ -23,6 +23,16 @@ angular.module('crucio')
 
         $scope.collectionID = $scope.collectionid;
         $scope.method = 'question';
+
+        if ($scope.exam) {
+          $scope.collection = {};
+          $scope.collection.info = $scope.exam;
+          $scope.collection.info.type = 'exam';
+          $scope.collection.question_id_list = [];
+          for (var i = 0; i < $scope.exam.question_count; i++) {
+            $scope.collection.question_id_list.push(0);
+          }
+        }
       },
       templateUrl: 'app/components/collection-card/collection-card.html',
       transclude: true
