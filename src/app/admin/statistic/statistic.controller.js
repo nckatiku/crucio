29d000874e96f64bc557c0a0cf76d92366f17f58
Lars <lbersch@gwdg.de>
2015-06-12 12:37:57 +0200

    Added live switch in admin statistic

--- src/app/admin/statistic/statistic.controller.js
+++ src/app/admin/statistic/statistic.controller.js
@@ -1,8 +1,19 @@
 'use strict';
 
 angular.module('crucio')
-  .controller('AdminStatisticCtrl', function ($scope, API) {
-    API.get('/statistic').success(function(data) {
-    	$scope.statistic = data.statistic;
-    });
+  .controller('AdminStatisticCtrl', function ($scope, $interval, API) {
+    $scope.reload = function() {
+      API.get('/statistic').success(function(data) {
+      	$scope.statistic = data.statistic;
+      });
+    };
+
+    $scope.checkAutoReload = function() {
+      if ($scope.autoReload) {
+        $scope.reload();
+      }
+    };
+
+    $scope.reload();
+    $interval($scope.checkAutoReload, 2000);
   });
